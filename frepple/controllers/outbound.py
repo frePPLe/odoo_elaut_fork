ee8c3c020220afe4cc3dbb5c0c8dfac58bc2392c
Johan De Taeye <jdetaeye@frepple.com>
2025-09-03 15:28:11 +0200

    fix for MTO sales orders

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -1962,8 +1962,8 @@ class exporter(object):
 
         def getReservedQuantity(stock_move_id):
             reserved_quantity = 0
-            if stock_move_id in stock_moves_dict:
-                mv = stock_moves_dict[stock_move_id]
+            mv = stock_moves_dict.get(stock_move_id, None)
+            if mv and mv["procure_method"] != "make_to_order":
                 reserved_quantity = mv["quantity"] or 0
                 for i in mv["move_orig_ids"]:
                     if i != stock_move_id:
