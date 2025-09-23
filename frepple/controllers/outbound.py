c16ea83021ec5621bdf1461d8627714cb6f442a3
Johan De Taeye <jdetaeye@frepple.com>
2025-09-23 13:57:35 +0200

    Adding extra field "quantity multiple" on the BOM

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -1396,6 +1396,7 @@ class exporter(object):
                 "days_to_prepare_mo",
                 "sequence",
                 "code",
+                "product_qty_multiple",
             ],
         ):
             # Determine the location
@@ -1497,6 +1498,16 @@ class exporter(object):
                                 quoteattr(product_buf["name"]),
                                 quoteattr(location),
                             )
+
+                        # Handle multiple quantity of a bom (frepple custom extra field)
+                        if i.get("product_qty_multiple", 0) > 0:
+                            multipleQty = self.convert_qty_uom(
+                                i["product_qty_multiple"],
+                                i["product_uom_id"],
+                                i["product_tmpl_id"][0],
+                            )
+                            if multipleQty > 0:
+                                yield "<size_multiple>%s</size_multiple>\n" % multipleQty
 
                         # Handle produced quantity of a bom
                         producedQty = self.convert_qty_uom(
@@ -1670,6 +1681,16 @@ class exporter(object):
                             quoteattr(product_buf["name"]),
                             quoteattr(location),
                         )
+
+                        # Handle multiple quantity of a bom (frepple custom extra field)
+                        if i.get("product_qty_multiple", 0) > 0:
+                            multipleQty = self.convert_qty_uom(
+                                i["product_qty_multiple"],
+                                i["product_uom_id"],
+                                i["product_tmpl_id"][0],
+                            )
+                            if multipleQty > 0:
+                                yield "<size_multiple>%s</size_multiple>\n" % multipleQty
 
                         # Handle produced quantity of a bom
                         producedQty = (
