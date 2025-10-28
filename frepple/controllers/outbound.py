ea38a5ec0ad51bb6ef08ca281d5cd84ac6207069
Johan De Taeye <jdetaeye@frepple.com>
2025-10-28 12:53:17 +0100

    Improved logic to link MO to the correct SO.

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2509,7 +2509,8 @@ class exporter(object):
 
             # Get MTO link
             mto_so = (
-                i.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id
+                i.sale_line_id.order_id.ids
+                + i.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id
             )
             if mto_so:
                 batch = mto_so[0].name
