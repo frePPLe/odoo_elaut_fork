e88a59ac3ea7da10c77dcd23e0aa1da6e6b89d46
Johan De Taeye <jdetaeye@frepple.com>
2026-01-20 14:39:39 +0100

    bug fix of calculation of reserved quantities

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2079,18 +2079,22 @@ class exporter(object):
                     "product_uom_qty",
                     "product_uom",
                     "state",
+                    "move_line_ids",
                 ],
             )
         }
 
-        def getReservedQuantity(stock_move_id):
+        def getReservedQuantity(sm):
             reserved_quantity = 0
-            mv = stock_moves_dict.get(stock_move_id, None)
-            if mv and mv["procure_method"] != "make_to_order":
-                reserved_quantity = mv["quantity"] or 0
-                for i in mv["move_orig_ids"]:
-                    if i != stock_move_id:
-                        reserved_quantity += getReservedQuantity(i)
+            for i in self.generator.getData(
+                "stock.move.line",
+                ids=sm["move_line_ids"],
+                search=[("state", "not in", ("done", "cancel"))],
+                fields=[
+                    "quantity_product_uom",
+                ],
+            ):
+                reserved_quantity += i["quantity_product_uom"] or 0
             return reserved_quantity
 
         # Generate the demand records
@@ -2158,7 +2162,7 @@ class exporter(object):
                                 self.product_product[i["product_id"][0]]["template"],
                             )
                             reserved_quantity = (
-                                getReservedQuantity(mv_id)
+                                getReservedQuantity(sm)
                                 if self.respect_reservations
                                 else 0
                             )
@@ -2521,21 +2525,41 @@ class exporter(object):
                 )
             ]
             # a second call to get the reserved quantities
-            reserved_quantity = {}
-            for i in self.generator.getData(
+            moves = self.generator.getData(
                 "stock.move",
                 search=[
                     ("state", "in", ["partially_available", "assigned"]),
                     ("production_id", "=", False),
                     ("workorder_id", "=", False),
-                    ("origin", "in", confirmed_mos),
+                    ("raw_material_production_id", "in", confirmed_mos),
                 ],
-                fields=["origin", "product_id", "quantity"],
-            ):
-                reserved_quantity[(i["origin"], i["product_id"][0])] = (
-                    reserved_quantity.get((i["origin"], i["product_id"][0]), 0)
-                    + i["quantity"]
-                )
+                fields=["raw_material_production_id", "product_id", "move_line_ids"],
+            )
+            all_line_ids = []
+            for m in moves:
+                all_line_ids.extend(m["move_line_ids"])
+            reserved_quantity = {}
+            if all_line_ids:
+                line_qty_map = {
+                    l["id"]: l["quantity"]
+                    for l in self.generator.getData(
+                        "stock.move.line",
+                        search=[("id", "in", list(set(all_line_ids)))],
+                        fields=["quantity"],
+                    )
+                }
+                for m in moves:
+                    for line_id in m["move_line_ids"]:
+                        reserved_quantity[
+                            (m["raw_material_production_id"][1], m["product_id"][0])
+                        ] = reserved_quantity.get(
+                            (m["raw_material_production_id"][1], m["product_id"][0]), 0
+                        ) + line_qty_map.get(
+                            line_id, 0
+                        )
+            # Release temp variables
+            all_line_ids = None
+            moves = None
 
         yield "<!-- manufacturing orders in progress -->\n"
         yield "<operationplans>\n"
