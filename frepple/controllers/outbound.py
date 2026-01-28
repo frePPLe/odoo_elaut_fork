116ca18e2d899215145521d26e697022238b2538
Johan De Taeye <jdetaeye@frepple.com>
2026-01-28 17:34:01 +0100

    fix MTO links on PO and MO in multi level MTO chains

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2384,9 +2384,15 @@ class exporter(object):
                         mto_so = mv.move_dest_ids.group_id.sale_id
                         batch = mto_so[0].name if mto_so else None
                         if not batch:
-                            mto_mo = j._get_mrp_productions()
-                            if mto_mo:
-                                batch = mto_mo[0].display_name
+                            # Follow multi-level MTO chain to the sale order
+                            for mo in j._get_mrp_productions():
+                                mto_so = (
+                                    mo.procurement_group_id.sale_id
+                                    + mo.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id
+                                )
+                                if mto_so:
+                                    batch = mto_so[0].name
+                                    break
                     else:
                         batch = None
 
@@ -2489,9 +2495,15 @@ class exporter(object):
                         mto_so = i.move_dest_ids.group_id.sale_id
                         batch = mto_so[0].name if mto_so else None
                         if not batch:
-                            mto_mo = j._get_mrp_productions()
-                            if mto_mo:
-                                batch = mto_mo[0].display_name
+                            # Follow multi-level MTO chain to the sale order
+                            for mo in j._get_mrp_productions():
+                                mto_so = (
+                                    mo.procurement_group_id.sale_id
+                                    + mo.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id
+                                )
+                                if mto_so:
+                                    batch = mto_so[0].name
+                                    break
                     else:
                         batch = None
 
@@ -2627,16 +2639,21 @@ class exporter(object):
                 i.procurement_group_id.sale_id
                 + i.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id
             )
-            if mto_so:
-                batch = mto_so[0].name
-            else:
-                mto_mo = i._get_sources()
-                batch = mto_mo[0].display_name if mto_mo else i.name
+            batch = mto_so[0].name if mto_so else None
+            if not batch:
+                for mo in i._get_sources():
+                    mto_so = (
+                        mo.procurement_group_id.sale_id
+                        + mo.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id
+                    )
+                    if mto_so:
+                        batch = mto_so[0].name
+                        break
 
             # Create a record for the MO
-            yield '<operationplan type="MO" reference=%s batch=%s %s="%s" quantity="%s" status="%s">\n' % (
+            yield '<operationplan type="MO" reference=%s %s%s="%s" quantity="%s" status="%s">\n' % (
                 quoteattr(i.name),
-                quoteattr(batch),
+                "batch=%s " % quoteattr(batch) if batch else "",
                 (
                     "start"  # Option 1: compute MO end date based on the start date
                     if self.manage_work_orders or not enddate
