15a5c3c1ffa6b07a09d0ed21af9e522836027d0c
Johan De Taeye <jdetaeye@frepple.com>
2026-02-09 11:50:03 +0100

    improved assigning of batch field

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2419,6 +2419,9 @@ class exporter(object):
                                 if mto_so:
                                     batch = mto_so[0].name
                                     break
+                        if not batch:
+                            # A PO for a MTO product was created without a sales order link.
+                            batch = j.name
                     else:
                         batch = None
 
@@ -2533,6 +2536,9 @@ class exporter(object):
                                 if mto_so:
                                     batch = mto_so[0].name
                                     break
+                            if not batch:
+                                # A PO for a MTO product was created without a sales order link.
+                                batch = j.name
                     else:
                         batch = None
 
@@ -2629,6 +2635,16 @@ class exporter(object):
                     if mto_so:
                         batch = mto_so[0].name
                         break
+            try:
+                if (
+                    not batch
+                    and self.route_mto
+                    in self.product_product[i.product_id.id]["template"]["route_ids"]
+                ):
+                    # A MO for a MTO product was created without a sales order link.
+                    batch = i.name
+            except Exception:
+                pass
 
             # Create a record for the MO
             yield '<operationplan type="MO" reference=%s %s%s="%s" quantity="%s" status="%s">\n' % (
