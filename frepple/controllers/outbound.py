9084769f2354fa25eeb36eac3a2d566f4c0454dd
Johan De Taeye <jdetaeye@frepple.com>
2026-02-09 11:10:53 +0100

    revert exclusion of done purchase orders

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2359,7 +2359,7 @@ class exporter(object):
                         # Alternative I: don't send RFQs to frepple because that supply isn't certain to be available yet.
                         # ("draft", "sent", "bid", "to approve", "confirmed", "cancel"),
                         # Alternative II: send RFQs to frepple to avoid that the same purchasing proposal is generated again by frepple.
-                        ("bid", "confirmed", "cancel", "done"),
+                        ("bid", "confirmed", "cancel"),
                     ),
                     ("order_id.state", "=", False),
                     "|",
