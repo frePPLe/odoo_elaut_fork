53a5d8639dfd812a869c25640563c3c873701172
Johan De Taeye <jdetaeye@frepple.com>
2026-02-11 12:08:18 +0100

    updated po filter to avoid endless recursion

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2327,22 +2327,29 @@ class exporter(object):
         'confirmed' -> operationplan.status
         """
 
-        def getRemainingQuantity(sm, target_uom):
+        def getRemainingQuantity(sm, target_uom, move_chain=None):
             if sm.state == "cancel":
                 return 0.0
             po_specific_dest_moves = sm.move_dest_ids.filtered(
                 lambda m: not m.raw_material_production_id
                 and not m.sale_line_id
-                and m.id != sm.id
+                and not m.origin_returned_move_id
+                and not m.returned_move_ids
+                # Generic protection against infinite recursion in move cycles
+                and (
+                    (move_chain and m.id not in move_chain)
+                    or (not move_chain and m.id != sm.id)
+                )
             )
             if po_specific_dest_moves:
                 # A chain of destination moves within the PO that needs to be recursed
-                logger.error(
-                    "FREPPLE.DEBUG: remaining quantity for stock move %s"
-                    % sm.display_name
-                )
+                if move_chain:
+                    move_chain_copy = move_chain + [sm.id]
+                else:
+                    move_chain_copy = [sm.id]
                 return sum(
-                    getRemainingQuantity(m, target_uom) for m in po_specific_dest_moves
+                    getRemainingQuantity(m, target_uom, move_chain_copy)
+                    for m in po_specific_dest_moves
                 )
             elif sm.state == "done":
                 # End of a chain, and the material is already booked in stock
