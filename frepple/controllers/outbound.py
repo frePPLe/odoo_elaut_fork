269f9ad1292405bc62e79a6a27c07ce9ae63a741
Johan De Taeye <jdetaeye@frepple.com>
2026-02-19 14:54:02 +0100

    a f***ing copy-paste typo in mto logic

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2587,7 +2587,7 @@ class exporter(object):
             batch = mto_so[0].name if mto_so else None
             if batch:
                 return batch
-            for related_mo in i._get_sources():
+            for related_mo in mo._get_sources():
                 if not mo_chain:
                     batch = getBatch(related_mo, [mo.id])
                 elif related_mo.id not in mo_chain:
