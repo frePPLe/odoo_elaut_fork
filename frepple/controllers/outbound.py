3234a569fbe49fecc65a9afce71a52b06452c9cc
Johan De Taeye <jdetaeye@frepple.com>
2026-03-04 13:44:49 +0100

    reviewed quantity to use for MO of serialized production

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2692,14 +2692,10 @@ class exporter(object):
             if i.product_id.tracking in ["serial", "lot"]:
                 # Tracking by lot or unique serial number requires that we track
                 # the production intent.
-                qty = sum(
-                    move.product_uom_id._compute_quantity(
-                        move.product_qty, i.product_id.uom_id
-                    )
-                    for move in i.procurement_group_id.mrp_production_ids.filtered(
-                        lambda m: m.product_id == i.product_id
-                        and m.state not in ["done", "cancel"]
-                    )
+                self.convert_qty_uom(
+                    i.product_qty,
+                    i.product_uom_id.id,
+                    self.product_product[i.product_id.id]["template"],
                 )
             else:
                 # No serialization on the product
