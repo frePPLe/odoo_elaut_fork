37992bcd8010a697432c48c868c25c8958be370c
Johan De Taeye <jdetaeye@frepple.com>
2026-06-04 15:52:10 +0200

    revised logic to separate logical(mto) and physical reservations

--- frepple/controllers/outbound.py
+++ frepple/controllers/outbound.py
@@ -2998,21 +2998,7 @@ class exporter(object):
                             )
                     if self.respect_reservations:
                         for l in mv.move_line_ids | mv.move_orig_ids.move_line_ids:
-                            if (
-                                # Normal reservation case
-                               mv.procure_method != "make_to_order"
-                                and l.state == "assigned"
-                            ) or (
-                                # Special case for multi-level MTO chains
-                                mv.procure_method == "make_to_order"
-                                and l.move_id.state
-                                not in (
-                                    "waiting",
-                                    "waiting availability",
-                                    "available",
-                                    "partially_available",
-                                )
-                            ):
+                            if l.state == "assigned" and l.move_id.picking_id:
                                 qty_flow -= l.product_uom_id._compute_quantity(
                                     l.quantity, default_uom
                                 )
@@ -3122,19 +3108,7 @@ class exporter(object):
                                 )
                         if self.respect_reservations:
                             for l in mv.move_line_ids | mv.move_orig_ids.move_line_ids:
-                                if (
-                                    mv.procure_method != "make_to_order"
-                                    and l.state == "assigned"
-                                ) or (
-                                    mv.procure_method == "make_to_order"
-                                    and l.move_id.state
-                                    not in (
-                                        "waiting",
-                                        "waiting availability",
-                                        "available",
-                                        "partially_available",
-                                    )
-                                ):
+                                if l.state == "assigned" and l.move_id.picking_id:
                                     qty_flow -= l.product_uom_id._compute_quantity(
                                         l.quantity, default_uom
                                     )
